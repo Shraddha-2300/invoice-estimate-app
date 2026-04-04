3d246cb66088d7ec228635d2cb74d6a04740b4cc
Shraddha-2300 <shraddhaisnothere@gmail.com>
2026-04-05 00:38:05 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import { useState } from "react";
-import Form from "./components/Form";
-import Preview from "./components/Preview";
-
 function App() {
-  const [data, setData] = useState({});
-
+  
   return (
     <div>
-      <Form setData={setData} />
-      <Preview data={data} />
+     <h3>I love you Dev...</h3>
     </div>
   );
 }
